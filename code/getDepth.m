7707ff9e9b27a2a408efc3634d39d0b3cfd440f3
Vibhavi Peiris <vibi9505@gmail.com>
2017-11-17 18:35:12 -0500

    Revert "Updating From master"

--- code/getDepth.m
+++
@@ -1,6 +0,0 @@
-function depth = getDepth(im)
-    calib = getData(im, 'test', 'calib');
-    image = getData(im, 'test', 'disp');
-    f = calib.f * calib.baseline;
-    depth = f./image.disparity;
-end
--- code/main.m
+++
@@ -1,9 +0,0 @@
-addpath(genpath('dpm'));
- 
-%  detect cars in n images and save as ds
-q2b(20);
- 
-%  train a classifier
-
-
-%  plot arrows
